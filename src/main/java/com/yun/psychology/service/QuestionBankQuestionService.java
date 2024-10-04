45be0de2a61d7c174b68211e361451b0f29c167d
Yuan <497796460@qq.com>
2024-10-04 20:08:29 +0800

    del branch

--- src/main/java/com/yun/psychology/service/QuestionBankQuestionService.java
+++ src/main/java/com/yun/psychology/service/QuestionBankQuestionService.java
@@ -1,13 +1,53 @@
 package com.yun.psychology.service;
 
-import com.yun.psychology.model.entity.QuestionBankQuestion;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.yun.psychology.model.dto.questionbankquestion.QuestionBankQuestionQueryRequest;
+import com.yun.psychology.model.entity.QuestionBankQuestion;
+import com.yun.psychology.model.vo.QuestionBankQuestionVO;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
-* @author Yuan
-* @description 针对表【question_bank_question(题库题目)】的数据库操作Service
-* @createDate 2024-10-04 16:42:54
-*/
+ * 题库题目关联服务
+ *
+ * @author <a href="https://github.com/aguumon">YuanXiao</a>
+ * @from <a href="https://www.code-nav.cn">编程导航学习圈</a>
+ */
 public interface QuestionBankQuestionService extends IService<QuestionBankQuestion> {
 
+    /**
+     * 校验数据
+     *
+     * @param questionBankQuestion
+     * @param add 对创建的数据进行校验
+     */
+    void validQuestionBankQuestion(QuestionBankQuestion questionBankQuestion, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param questionBankQuestionQueryRequest
+     * @return
+     */
+    QueryWrapper<QuestionBankQuestion> getQueryWrapper(QuestionBankQuestionQueryRequest questionBankQuestionQueryRequest);
+    
+    /**
+     * 获取题库题目关联封装
+     *
+     * @param questionBankQuestion
+     * @param request
+     * @return
+     */
+    QuestionBankQuestionVO getQuestionBankQuestionVO(QuestionBankQuestion questionBankQuestion, HttpServletRequest request);
+
+    /**
+     * 分页获取题库题目关联封装
+     *
+     * @param questionBankQuestionPage
+     * @param request
+     * @return
+     */
+    Page<QuestionBankQuestionVO> getQuestionBankQuestionVOPage(Page<QuestionBankQuestion> questionBankQuestionPage, HttpServletRequest request);
 }
